5d8306cb707da4bf969478e13c8622d6b8773d83
Oscar-AVS <alexandervilchis638@gmail.com>
2025-05-29 00:04:44 -0600

    Update ProfeBenji.h

--- ProfeBenji.h
+++ ProfeBenji.h
@@ -1,6 +1,7 @@
 // Este es el progrma creado para la clase "ProfeBenji.h" En esta clase se crea al personaje de ProfeBenji, esta clase representa al profesor este profe reacciona a las decisiones que toma el jugador 
 // Creado por: Oscar Alexander Vilchis Soto (A01713207)
 // Creado el 20/05/2025
+// Modificado el 28/05/2025 Para el tercer avance igual que a la clase Jugado, se añadió el método `reaccionar()` como sobrescritura  del método virtual en la clase base "Personaje"
 
 
 #ifndef PROFEBENJI_H
@@ -39,7 +40,9 @@ public:
     string getEstadoAnimo() const { return estadoAnimo; }
 
     string getFrase() const { return frase; }
+    void reaccionar() override {
+        cout << "Este alumno me deja  " <<  ": " << estadoAnimo << ", " << frase << endl;
+    }
 };
 
-// Fin del "include guard", asegura que este archivo no se incluya más de una vez.
 #endif
