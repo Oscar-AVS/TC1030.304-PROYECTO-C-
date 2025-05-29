0afdf6690ba5010709f01868833b6ce97209594a
Oscar-AVS <alexandervilchis638@gmail.com>
2025-05-29 00:08:52 -0600

    Update RegistroXP.h

--- RegistroXP.h
+++ RegistroXP.h
@@ -1,3 +1,10 @@
+// Este es el programa creado para la clase "RegistroXP.h". Esta clase sirve como un controlador para modificar y mostrar los puntos de experiencia (XP) del jugador.
+// Creado por: Oscar Alexander Vilchis Soto (A01713207)
+// Creado el 20/05/2025
+
+// Modificado el 28/05/2025 de igual manera en esta clase no fue necesario hacer algún cambio
+
+
 #ifndef REGISTROXP_H
 #define REGISTROXP_H
 
